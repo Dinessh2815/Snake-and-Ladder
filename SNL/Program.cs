275119262f6a547f5db01347c9fe4dc5863a252f
Dinessh2815 <dinessh.venkat28@gmail.com>
2025-11-07 16:53:29 +0530

    completed uc2 with only arrays

--- SNL/Program.cs
+++ SNL/Program.cs
@@ -4,28 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+
             int[] board = new int[101];
-            
-            for(int i = 1; i <= 100; i++)
+
+            for (int i = 1; i <= 100; i++)
             {
                 board[i] = i;
             }
 
 
-            Dictionary<int, int> ladders = new Dictionary<int, int>
+            int[,] ladders = new int[,]
             {
-                { 4, 14 },   
-                { 9, 31 },   
-                { 20, 38 },  
-                { 28, 84 },  
-                { 40, 59 },  
-                { 51, 67 },  
-                { 63, 81 },  
-                { 71, 91 }   
+                { 4, 14 },
+                { 9, 31 },
+                { 20, 38 },
+                { 28, 84 },
+                { 40, 59 },
+                { 51, 67 },
+                { 63, 81 },
+                { 71, 91 }
             };
 
-            Dictionary<int, int> snakes = new Dictionary<int, int>
+            int[,] snakes = new int[,]
             {
                 { 17, 7 },
                 { 54, 34 },
@@ -37,21 +37,15 @@
                 { 99, 78 }
             };
 
-
-            foreach (var ladder in ladders)
-            {
-                board[ladder.Key] = ladder.Value;
-            }
-            foreach (var snake in snakes)
-            {
-                board[snake.Key] = snake.Value;
-            }
             int position = 0;
-            
-            Random random = new Random();
-            int die = random.Next(1, 7);
-            Console.WriteLine("Die rolled: " + die);
-            
+            while (position <= 100)
+            {
+                Random random = new Random();
+                int die = random.Next(1, 7);
+                Console.WriteLine("Die rolled: " + die);
+
+            }
+
 
         }
     }
